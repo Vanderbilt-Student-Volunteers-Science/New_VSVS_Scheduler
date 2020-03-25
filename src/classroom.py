0f8699f15bf3eaf416a6c0270af3d9dc9901b6b5
David <davidewardjr@digital.net>
2020-03-24 21:37:24 -0500

    Added comments and school travel time global constant

--- src/classroom.py
+++ src/classroom.py
@@ -1,8 +1,9 @@
 import src.convertSchedule
 
-TIME = 45
+SCHOOL_TRAVEL_TIME = 45  # one-way, in minutes
 
 class Classroom:
+
     def __init__(self, group_number, teacher_name, teacher_phone, school, teacher_email, class_start_time, class_end_time, day_of_week):
         self.group_number = group_number
         self.teacher_name = teacher_name
@@ -16,24 +17,15 @@ class Classroom:
         self.t_leader = 0  # has a team leader?
         self.driver = 0  # has a driver?
 
-        #  maybe a better way to do this? don't want to put if statements in both convertSchedule methods TODO: add method combining free_time_start and start_time_schedule_index
-        if self.school == "Head":
-            self.free_time_start = src.convertSchedule.calculate_free_time_start(self.class_start_time, HEAD)
-            self.volunteer_time_needed = src.convertSchedule.calculate_free_time_needed(self.class_start_time, self.class_end_time, HEAD)
-        elif self.school == "H.G. Hill":
-            self.free_time_start = src.convertSchedule.calculate_free_time_start(self.class_start_time, HILL)
-            self.volunteer_time_needed = src.convertSchedule.calculate_free_time_needed(self.class_start_time, self.class_end_time, HILL)
-        else:
-            self.free_time_start = src.convertSchedule.calculate_free_time_start(self.class_start_time, OTHER)
-            self.volunteer_time_needed = src.convertSchedule.calculate_free_time_needed(self.class_start_time, self.class_end_time, OTHER)
+        # the latest time a volunteer can start being available and be able to make it to the beginning of a lesson
+        self.free_time_start = src.convertSchedule.calculate_free_time_start(self.class_start_time, SCHOOL_TRAVEL_TIME)
 
-        self.start_time_schedule_index = src.convertSchedule.military_to_schedule_array(self.day_of_week, self.free_time_start) # the index in the array of the volunteer schedule that needs to be volunteer_time_needed for a volunteer to be able to visit this classroom
+        # free time needed starting at free_time_start for a volunteer to be able to make the end of the lesson
+        self.volunteer_time_needed = src.convertSchedule.calculate_free_time_needed(self.class_start_time, self.class_end_time, SCHOOL_TRAVEL_TIME)
 
-        self.assignment_priority = 0  # vector of the people that can make this classroom
+        # the index in the array of the volunteer schedule that needs to be >= volunteer_time_needed for a volunteer to be able to visit this classroom
+        self.start_time_schedule_index = src.convertSchedule.military_to_schedule_array(self.day_of_week, self.free_time_start)
 
 
     def add_volunteer(self):
         self.volunteers_assigned += 1
-
-    def add_to_assignment_priority(self):
-        self.assignment_priority += 1
