f40f9916c6fa545542ebb89c6fac4b4ae61e0fab
David <davidewardjr@digital.net>
2020-04-11 23:46:21 -0400

    Added thorough comments in classroom.py

--- src/classroom.py
+++ src/classroom.py
@@ -10,22 +10,37 @@ class Classroom:
         self.teacher_phone = teacher_phone
         self.school = school
         self.teacher_email = teacher_email
-        self.class_start_time = src.convertSchedule.convert_to_military(class_start_time)
-        self.class_end_time = src.convertSchedule.convert_to_military(class_end_time)
         self.day_of_week = day_of_week
         self.volunteers_assigned = 0
-        self.t_leader = 0  # has a team leader?
-        self.driver = 0  # has a driver?
 
-        # the latest time a volunteer can start being available and be able to make it to the beginning of a lesson
+        # has a team leader?
+        self.t_leader = 0
+
+        # has a driver?
+        self.driver = 0
+
+        # time the class starts in military time
+        self.class_start_time = src.convertSchedule.convert_to_military(class_start_time)
+
+        # time the class ends in military time
+        self.class_end_time = src.convertSchedule.convert_to_military(class_end_time)
+
+        # the latest time a volunteer can start being available and be able to make this lesson
         self.free_time_start = src.convertSchedule.calculate_free_time_start(self.class_start_time, src.globalAttributes.SCHOOL_TRAVEL_TIME)
 
-        # free time needed starting at free_time_start for a volunteer to be able to make the end of the lesson
+        # minutes of free time needed starting at free_time_start for a volunteer to be able to make this lesson
         self.volunteer_time_needed = src.convertSchedule.calculate_free_time_needed(self.class_start_time, self.class_end_time, src.globalAttributes.SCHOOL_TRAVEL_TIME)
 
-        # the index in the array of the volunteer schedule that needs to be >= volunteer_time_needed for a volunteer to be able to visit this classroom
+        # the index in the array of Volunteer attribute volunteer_schedule (or partner_schedule) that needs to be
+        # >= volunteer_time_needed for a volunteer to be able to visit this classroom
         self.start_time_schedule_index = src.convertSchedule.military_to_schedule_array(self.day_of_week, self.free_time_start)
 
+    # Assigns a volunteer to a classroom. Updates the Volunteer group_number attribute with the group number of the
+    # classroom the volunteer is being assigned to and the Classroom object with a new volunteers_assigned value. If
+    # the classroom doesn't have a team leader or driver and the volunteer is one of those, sets the Classroom t_leader
+    # and Volunteer assigned_t_leader attributes or the Classroom driver and Volunteer assigned_driver attributes.
+    # self -      Classroom object volunteer is being assigned to
+    # volunteer - volunteer being assigned to self Classroom object
     def assign_volunteer(self, volunteer):
         self.volunteers_assigned += 1
         volunteer.set_group_number(self.group_number)
@@ -35,3 +50,4 @@ class Classroom:
         if self.t_leader == 0 and volunteer.applied_t_leader:
             self.t_leader = 1
             volunteer.assign_t_leader()
+            
--- src/volunteer.py
+++ src/volunteer.py
@@ -22,7 +22,7 @@ class Volunteer:
             self.car_passengers = int(car_passengers)
         self.driver = (self.car_passengers >= src.globalAttributes.MAX_TEAM_SIZE)  # can drive if needed
         self.schedule_array = src.convertSchedule.convert_to_schedule_array(imported_schedule)  # array of 1's and 0's
-        self.schedule = src.convertSchedule.convert_schedule_array(self.schedule_array)  # array of minutes of consecutive free time after each time
+        self.volunteer_schedule = src.convertSchedule.convert_schedule_array(self.schedule_array)  # array of minutes of consecutive free time after each time
         self.group_number = -1
         self.partners = 0  # Number of other partners (NOT including this Volunteer) Volunteer applied with, set in add_partners method. This is only set for the Volunteer that signed the partners up
         self.partner_indexes = []  # index of each partner in volunteer_list
