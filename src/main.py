f418b709bca70fc36bec06d60229c38f7d49c205
carmen904c <78514823+carmen904c@users.noreply.github.com>
2022-01-29 11:17:59 -0600

    Spring 2022 Rudimentary

--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ import src.assign
 import src.classroom
 import src.global_attributes
 from src.classroom import Classroom
-from src.global_attributes import INDIVIDUAL_INDEX, CLASSROOM_INDEX
+from src.global_attributes import VOLUNTEER_INDEX, CLASSROOM_INDEX
 from src.volunteer import Volunteer
-
 
 # All global constants and variables are in global_attributes.py
 
@@ -16,78 +15,39 @@ def custom_formatwarning(msg, *args, **kwargs):
     # ignore everything except the message
     return f"WARNING: {str(msg)}  \n"
 
-
 warnings.formatwarning = custom_formatwarning
-
 
 def main():
     #  IMPORT FILE DATA
-
     # import individual application data from individuals.csv
     with open('../data/individuals.csv', 'r') as individuals_csv:  # opens individuals.csv as individuals_csv
         csv_reader = csv.reader(individuals_csv, delimiter=',')  # divides individuals_csv by commas
         next(csv_reader)  # skips the header
-        for row in csv_reader:  # for each individual
 
-            # check vanderbilt email
-            if not ('vanderbilt' in row[INDIVIDUAL_INDEX['EMAIL']].strip().lower()):
+        for row in csv_reader:  # for each individual
+            # check that volunteer has vanderbilt email
+            if not ('vanderbilt' in row[VOLUNTEER_INDEX['EMAIL']].strip().lower()):
                 warnings.warn(
-                    f"{row[INDIVIDUAL_INDEX['FIRST']].strip()} {row[INDIVIDUAL_INDEX['LAST']].strip()} does not have a Vanderbilt email.")
+                    f"{row[VOLUNTEER_INDEX['FIRST']].strip()} {row[VOLUNTEER_INDEX['LAST']].strip()} does not have a Vanderbilt email.")
 
             # pull data from row in the csv, create a Volunteer object, and add it to global variable
             # volunteer_list
-            # I is a dictionary containing all the indices for each field
+            # INDIVIDUAL_INDEX is a dictionary containing all the indices for each field
             volunteer = Volunteer(
-                first=row[INDIVIDUAL_INDEX['FIRST']].strip(),
-                last=row[INDIVIDUAL_INDEX['LAST']].strip(),
-                phone=row[INDIVIDUAL_INDEX['PHONE']].strip(),
-                email=row[INDIVIDUAL_INDEX['EMAIL']].strip().lower(),
-                year_in_school=row[INDIVIDUAL_INDEX['YEAR']].strip(),
-                major=row[INDIVIDUAL_INDEX['MAJOR']].strip(),
-                robotics_interest=False,  # no robotics for Spring 2021
-                special_needs_interest=(lambda x: True if x == 'Yes' else False)(
-                    row[INDIVIDUAL_INDEX['SPECIAL_NEEDS_INTEREST']].strip()),
+                first=row[VOLUNTEER_INDEX['FIRST']].strip(),
+                last=row[VOLUNTEER_INDEX['LAST']].strip(),
+                phone=row[VOLUNTEER_INDEX['PHONE']].strip(),
+                email=row[VOLUNTEER_INDEX['EMAIL']].strip().lower(),
+                year_in_school=row[VOLUNTEER_INDEX['YEAR']].strip(),
+                major=row[VOLUNTEER_INDEX['MAJOR']].strip(),
                 applied_t_leader=(lambda x: True if x == 'Yes' else False)(
-                    row[INDIVIDUAL_INDEX['APPLIED_T_LEADER']].strip()),
-                board=(lambda x: True if x == 'Yes' else False)(row[INDIVIDUAL_INDEX['BOARD']].strip()),
-                driver=False,  # no drivers for Spring 2021
-                car_passengers='',  # no drivers/cars for Spring 2021
-                imported_schedule=row[INDIVIDUAL_INDEX['IMPORTED_SCHEDULE_START']:INDIVIDUAL_INDEX[
-                                                                                      'IMPORTED_SCHEDULE_END'] + 1],
-                # location column in csv is either 'On-campus' or 'Remote', so we need to convert to boolean
-                is_in_person=(lambda x: True if x == 'On-campus' else False)(row[INDIVIDUAL_INDEX['LOCATION']].strip())
+                    row[VOLUNTEER_INDEX['APPLIED_T_LEADER']].strip()),
+                board=(lambda x: True if x == 'Yes' else False)(row[VOLUNTEER_INDEX['BOARD']].strip()),
+                schedule=row[VOLUNTEER_INDEX['SCHEDULE_START']:VOLUNTEER_INDEX['SCHEDULE_END'] + 1]
             )
             src.global_attributes.volunteer_list.append(volunteer)
 
     print('There are {} volunteers.'.format(len(src.global_attributes.volunteer_list)))
-
-    # import partner application data from partners.csv
-
-    with open('../data/partners.csv') as partners_csv:  # opens partners.csv as partners_csv
-        csv_reader = csv.reader(partners_csv, delimiter=',')  # divides partners_csv by commas
-        next(csv_reader)  # skip header row
-
-        for row in csv_reader:  # for each group of partners
-            first_partner_email = row[1].strip().lower()
-            # if the first partner submitted an individual application
-            for i, volunteer in enumerate(src.global_attributes.volunteer_list):
-                if first_partner_email == volunteer.email:
-                    first_partner = volunteer
-                    # add remaining partners to first partner
-                    first_partner.add_partners(*tuple(row[2:]))
-
-                    # sets partners, partner_indexes, and partner_free_time_array attributes for the Volunteer objects
-                    # of all other partners
-                    if first_partner.partners:
-                        for partner_index in first_partner.partner_indexes:
-                            partner = src.global_attributes.volunteer_list[partner_index]
-                            partner.partner_indexes = list(set(first_partner.partner_indexes) - {partner_index}) + [i]
-                            partner.partners = first_partner.partners
-                            partner.partner_free_time_array = first_partner.partner_free_time_array
-
-                else:
-                    warnings.warn(f'{first_partner_email} filled out a partner application but not an individual '
-                                  f'application.')
 
     # import classroom information from classrooms.csv
     with open('../data/classrooms.csv', 'r') as classrooms_csv:  # opens classrooms.csv as classrooms_csv
@@ -106,47 +66,33 @@ def main():
                                   class_end_time=row[CLASSROOM_INDEX['CLASS_END_TIME']].strip(),
                                   day_of_week=row[CLASSROOM_INDEX['DAY_OF_WEEK']].strip()
                                   )
-
             src.global_attributes.classroom_list.append(classroom)
+        print('There are {} classes.'.format(len(src.global_attributes.classroom_list)))
 
     # ASSIGN VOLUNTEERS
-
     # First assign board members and their partners
     for volunteer in src.global_attributes.volunteer_list:
         if volunteer.board and volunteer.group_number == -1:
-            if volunteer.partners:
-                src.assign.assign_partners(volunteer)
-                if volunteer.group_number == -1:
-                    src.assign.assign_single(volunteer)
-            else:
-                src.assign.assign_single(volunteer)
+            src.assign.assign_single(volunteer)
 
-    # print board members that can not be assigned
-    print("\n ------  BOARD MEMBERS THAT CAN NOT BE ASSIGNED ------")
+    # print board members that cannot be assigned
+    print("\n ------  BOARD MEMBERS THAT CANNOT BE ASSIGNED ------")
     for volunteer in src.global_attributes.volunteer_list:
         if volunteer.board and volunteer.group_number == -1:
             print(volunteer)
-    print("------------")
+    print("-----------------------------------------------------")
 
-    # assign partners
-    for volunteer in src.global_attributes.volunteer_list:
-        if volunteer.group_number == -1 and volunteer.partners:
-            src.assign.assign_partners(volunteer)  # adds all partners to same team
-
-    # for unassigned volunteers, count classrooms they can make, total is Volunteer attribute classrooms_possible
+    # count classrooms volunteers can make, total is Volunteer attribute classrooms_possible
+    print("\nVolunteers removed due to incompatible schedules:\n")
     for volunteer in src.global_attributes.volunteer_list:
         if volunteer.group_number == -1:
             for classroom in src.global_attributes.classroom_list:
                 if src.assign.volunteer_can_make_class(volunteer, classroom):
                     volunteer.classrooms_possible += 1
-
-    # make list of unassigned in person volunteers, sort them by the number of classrooms they can make
-    # (fewest to greatest number of classrooms they can make), then assign them to classroom groups
-    in_person_list = []
-    for volunteer in src.global_attributes.volunteer_list:
-        if volunteer.group_number == -1 and volunteer.is_in_person:
-            in_person_list.append(volunteer)
-    src.assign.assign_in_person(src.assign.sort_by_availability(in_person_list))
+            if not volunteer.classrooms_possible:
+                print(volunteer)
+                src.global_attributes.volunteer_list.remove(volunteer)
+    print("-----------------------------------------------------\n")
 
     # creates global variable lists of empty and partially filled classrooms
     for classroom in src.global_attributes.classroom_list:
@@ -173,17 +119,11 @@ def main():
             unsorted_list.append(volunteer)
     src.assign.assign_others(src.assign.sort_by_availability(unsorted_list))
 
-    # reassign volunteers that were assigned to groups of 1
-    # TODO: think through this part better
-    # FIXME: catch ValueError from reassigning volunteers if we are going to do this
-    unassigned_volunteers = []
-    for classroom in src.global_attributes.classroom_list:
-        if classroom.volunteers_assigned == 1:
-            unassigned_volunteers.extend(classroom.empty_classroom())
-    src.assign.assign_others(src.assign.sort_by_availability(unassigned_volunteers))
+    for volunteer in src.global_attributes.volunteer_list:
+        if volunteer.group_number == -1:
+            src.global_attributes.volunteer_list.remove(volunteer)
 
-    # OUTPUT RESULTS
-
+    # OUTPUT
     unassigned_volunteers = 0
 
     # create the results directory if it does not exist
@@ -202,7 +142,7 @@ def main():
 
         # Header row
         csv_writer.writerow(["Volunteer ID", "Group Number", "First Name", "Last Name", "Email", "Phone Number",
-                             "Year in School", " ", "Major", " ", "Driver", "Team Leader"])
+                             "Year in School", "Major", "Team Leader"])
 
         for volunteer_id, volunteer in enumerate(src.global_attributes.volunteer_list):
             if volunteer.group_number == -1:
@@ -216,10 +156,7 @@ def main():
                     volunteer.email,
                     volunteer.phone,
                     volunteer.year_in_school,
-                    '',
                     volunteer.major,
-                    '',
-                    int(volunteer.assigned_driver),  # convert True/False to 1/0
                     int(volunteer.assigned_t_leader)  # convert True/False to 1/0
                 ]
             )
@@ -234,9 +171,6 @@ def main():
         if volunteer.group_number != -1:
             classroom_counts[volunteer.group_number - 1] += 1
 
-    print(classroom_counts)
 
-
-# runs main
 if __name__ == '__main__':
     main()
